2ade704b9707fc6ad108edd2e52f1b4f9b5df6d1
cjsmith315 <cjsmith315@gmail.com>
2014-07-26 12:45:39 -0400

    Programming Assignment #2
    Added inverse matrix function logic

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix makes a matrix that stores functions that will be used by CacheSolve
+##to use: x<-matrix(); makeCacheMatrix(x)
+## 'x' can be a square or complex matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  v <- NULL
+  set <- function(y) {
+    x <<- y
+    v <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) v <<- inverse
+  getinverse <- function() v
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve uses the solve() function to invert 'x' which can be a square or complex matrix
+##logical matrices are coerced to numeric
+## ?solve to get R article
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+       
+        
+  
+  v <- x$getinverse()
+  if(!is.null(v)) {
+    message("getting cached data")
+    return(v)
+  }
+  data <- x$get()
+  v <- solve(data, ...)
+  x$setinverse(v)
+  v
 }
